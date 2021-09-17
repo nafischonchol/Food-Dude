21e0e477725b853410bcb3e1bb4f352418570019
nafischonchol <nafisislam161@gmail.com>
2021-09-17 15:30:55 +0600

    Create controller for order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    //
+    function index()
+    {
+        return view('iamrestaurant.orders.index');
+    }
+}
