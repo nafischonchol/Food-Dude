e2010337852bd46f14bcf5473068764be085efc5
nafischonchol <nafisislam161@gmail.com>
2021-09-17 15:29:06 +0600

    Reg page for resturant admin

--- resources/views/iamrestaurant/reg.blade.php
+++ resources/views/iamrestaurant/reg.blade.php
@@ -30,44 +30,60 @@
                             <div class="card-body p-0">
 
                                 <div class="col px-3 pt-3">
-                                    <form class="form-horizontal auth-form" id="frmregister" method="post" action="{{route('iamrestaurant-reg')}}">
+                                    <form class="form-horizontal auth-form" id="frmregister" method="post" action="{{ route('iamrestaurant-reg') }}" enctype="multipart/form-data">
                                         @csrf
+                                        @if (session('warning'))
+                                            <span class="alert alert-danger d-block" role="alert">
+                                                <strong>{{ session('warning') }}</strong>
+                                            </span>
+                                        @endif
+                                        @if (session('success'))
+                                            <span class="alert alert-success d-block" role="alert">
+                                                <strong>{{ session('success') }}</strong>
+                                            </span>
+                                        @endif
+                                         
                                         <div class="row">
+                                            
                                             <div class="col">
                                                 <div class="form-group mb-2">
-                                                    <label for="username">Mobile</label>
+                                                    <label for="username">Mobile <span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm"
-                                                            name="mobile" id="mobile" required
-                                                            data-parsley-minlength="11" placeholder="Min 11 chars.">
+                                                        <input type="text" value="{{ old('mobile') }}" class="form-control form-control-sm" name="mobile" id="mobile" required data-parsley-minlength="11" placeholder="Min 11 chars.">
                                                     </div>
                                                 </div>
 
                                                 <div class="form-group mb-2">
-                                                    <label for="owner">Owner Name</label>
+                                                    <label for="owner">Owner Name<span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="owner" id="owner" required placeholder="Owner Name">
+                                                        <input type="text" value="{{ old('owner') }}" class="form-control form-control-sm" name="owner" id="owner" required placeholder="Owner Name">
                                                     </div>
                                                 </div>
 
-                                                    
+
                                                 <div class="form-group mb-2">
-                                                    <label for="city">City</label>
+                                                    <label for="city">City<span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="city" id="city" placeholder="City">
+                                                        <input type="text" value="{{ old('city') }}" class="form-control form-control-sm" name="city" id="city" placeholder="City" required>
                                                     </div>
                                                 </div>
 
                                                 <div class="form-group mb-2">
                                                     <label for="location">Resturant Address</label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="location" id="location" placeholder="Resturant Address">
+                                                        <input type="text" value="{{ old('location') }}" class="form-control form-control-sm" name="location" id="location" placeholder="Resturant Address">
                                                     </div>
                                                 </div>
                                                 <div class="form-group mb-2">
-                                                    <label for="userpassword">Password</label>
+                                                    <label for="userpassword">Password<span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="password" class="form-control form-control-sm" name="pass" id="pass" placeholder="Password">
+                                                        <input type="password" class="form-control form-control-sm" name="pass" id="pass" placeholder="Password" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group mb-2">
+                                                    <label for="Image">Image<span class="text-danger">*</span></label>
+                                                    <div class="input-group">
+                                                        <input type="file" class="form-control form-control-sm" name="image" required>
                                                     </div>
                                                 </div>
                                                 <!--end form-group-->
@@ -80,57 +96,59 @@
                                                 <div class="form-group mb-2">
                                                     <label for="useremail">Email</label>
                                                     <div class="input-group">
-                                                        <input type="email" id="email" name="email" class="form-control form-control-sm" required parsley-type="email" placeholder="E-mail" />
+                                                        <input type="email" value="{{ old('email') }}" id="email" name="email" class="form-control form-control-sm" required parsley-type="email" placeholder="E-mail" />
                                                     </div>
                                                 </div>
                                                 <div class="form-group mb-2">
-                                                    <label for="res_name">Resturant Name</label>
+                                                    <label for="res_name">Resturant Name<span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="res_name" id="res_name" placeholder="Resturant Name">
+                                                        <input type="text"  value="{{ old('res_name') }}" class="form-control form-control-sm" name="res_name" id="res_name" placeholder="Resturant Name" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group mb-2">
                                                     <label for="res_name">Description</label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="description" id="description" placeholder="Resturant Description">
+                                                        <input type="text" value="{{ old('description') }}" class="form-control form-control-sm" name="description" id="description" placeholder="Resturant Description">
                                                     </div>
                                                 </div>
                                                 <div class="form-group mb-2 ml-2">
                                                     <label for="conf_password">Website</label>
                                                     <div class="input-group">
-                                                        <input type="text" class="form-control form-control-sm" name="website" id="website" placeholder="Enter website">
+                                                        <input type="text" value="{{ old('website') }}" class="form-control form-control-sm" name="website" id="website" placeholder="Enter website">
                                                     </div>
                                                 </div>
                                                 <div class="form-group mb-2 ml-2">
-                                                    <label for="conf_password">Confirm Password</label>
+                                                    <label for="conf_password">Confirm Password<span class="text-danger">*</span></label>
                                                     <div class="input-group">
-                                                        <input type="password" class="form-control form-control-sm" name="con_pass" id="con_pass" placeholder="Enter Confirm Password">
+                                                        <input type="password" class="form-control form-control-sm" name="con_pass" id="con_pass" placeholder="Enter Confirm Password" required>
                                                     </div>
                                                 </div>
-                                               
-                                                    <!--end col-->
-                                                </div>
-                                                <!--end form-group-->
-                                            </div>
-                                            <div class="form-group row my-3">
-                                                <div class="col-sm-12">
-                                                    <div class="custom-control custom-switch switch-success">
-                                                        <input type="checkbox" class="custom-control-input" checked id="isagree" name="isagree">
-                                                        <label class="custom-control-label text-muted" for="isagree">You agree to the Food Dude <a href="#" class="text-primary">Terms of Service</a></label>
-                                                    </div>
-                                                </div>
+                                                
                                                 <!--end col-->
                                             </div>
                                             <!--end form-group-->
-
-                                            <div class="form-group mb-0 row">
-                                                <div class="col-12">
-                                                    <button
-                                                        class="btn btn-primary btn-block waves-effect waves-light" type="submit">
-                                                        <span id="regspinner" class="mr-2" role="status" aria-hidden="true"></span>
-                                                        Register <i class="fas fa-sign-in-alt ml-1"></i>
-                                                    </button>
+                                        </div>
+                                        <div class="my-3 row">
+                                            <div class="col-7">
+                                                <div class="custom-control custom-switch switch-success">
+                                                    <input type="checkbox" class="custom-control-input" checked id="isagree" name="isagree">
+                                                    <label class="custom-control-label text-muted" for="isagree">You agree to the Food Dude <a href="#" class="text-primary">Terms of Service</a></label>
                                                 </div>
+                                            </div>
+                                            <div class="col-5">
+                                                <div class="text-right text-muted">
+                                                    <p class="mb-0">Have an account ? <a class="text-primary ml-2" href="{{ URL::route('iamrestaurant-login') }}"> Easy Login</a></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    
+
+                                        <div class="form-group mb-0 row">
+                                            <div class="col-12">
+                                                <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">
+                                                    <span id="regspinner" class="mr-2" role="status" aria-hidden="true"></span> Register <i class="fas fa-sign-in-alt ml-1"></i>
+                                                </button>
+                                            </div>
                                         </div>
                                     </form>
                                     <!--end form-->
@@ -139,7 +157,6 @@
                             <!--end card-body-->
                             <div class="card-body bg-light-alt text-center">
                                 <span class="text-muted d-none d-sm-inline-block">Food Dude © <?php echo date('Y'); ?></span>
-
                             </div>
 
                         </div>
