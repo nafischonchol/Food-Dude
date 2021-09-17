4024ef7da45fe4a75af1b37ca92cf0e527f4c169
nafischonchol <nafisislam161@gmail.com>
2021-09-17 17:07:47 +0600

    Hour migration created

---
+++ database/migrations/2021_09_17_100358_create_hours_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours', function (Blueprint $table) {
+            $table->id();
+            $table->integer('res_id');
+            $table->string('hours');
+            $table->string('tableQty');
+            $table->string('zactive')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours');
+    }
+}
