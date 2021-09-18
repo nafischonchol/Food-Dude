54f010e37dfc5411388c71696f6743681dd51ef8
nafischonchol <nafisislam161@gmail.com>
2021-09-18 21:55:22 +0600

    search resturant and show resturant

--- app/Http/Controllers/SearchRestaurantController.php
+++ app/Http/Controllers/SearchRestaurantController.php
@@ -4,16 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchRestaurantController extends Controller
 {
-    // function showall(){
-    //     $data = Restaurant::paginate(4);
-    //     return view('Searchingrestaurant',['allrestaurants'=>$data]);
+   
+    public function search(Request $req){
+      
+        
+        if(empty($req->date))
+            return back()->with('warning',"Please pick a date first!");
+      
+        if(empty($req->location))
+            $sql="select *,( select count(*) from orders where orders.res_id=restaurants.id ) as booked,(SELECT GROUP_CONCAT(hours) FROM `hours` where hours.res_id=restaurants.id group by res_id ) hours from restaurants where id in (SELECT res_id FROM `hours` where zactive=1 and tableQty>0 GROUP by res_id)";
+        else
+            $sql="select *,( select count(*) from orders where orders.res_id=restaurants.id ) as booked,(SELECT GROUP_CONCAT(hours) FROM `hours` where hours.res_id=restaurants.id group by res_id ) hours from restaurants where id in (SELECT res_id FROM `hours` where zactive=1 and tableQty>0 GROUP by res_id) and (location like '%".$req->location."%' or city like '%".$req->location."%' or res_name like '%".$req->location."%' or description like '%".$req->location."%')";
+        $data=DB::select($sql);
 
-    // }
-    public function sr(){
-        // dd('ss');
-        return view('Searchingrestaurant');
+        session()->put('date',$req->date);
+        session()->put('people',$req->people);
+
+        return view('searching.findrestaurant', ['restaurant'=>$data]);
     }
+
+    function com_reservation($res_id,$hour)
+    {
+        session()->put('reserve_hour',$hour);
+
+    
+
+    
+        $date=session('date');
+        $people=session('people');
+        $result=compact("date","hour","people");
+        $data=Restaurant::where('id',$res_id)->get();
+        
+        return view('searching.completereservation',['data'=>$data,'result'=>$result]);
+        
+    }
+
+ 
+
+    
 }
