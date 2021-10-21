0a14506578cb2761de547f5c0c62923213201318
nafischonchol <nafisislam161@gmail.com>
2021-10-22 04:46:54 +0600

    show waiting orders

---
+++ resources/views/iamrestaurant/orders/waiting.blade.php
@@ -0,0 +1,99 @@
+@extends('iamrestaurant.mainmenu')
+
+@section('meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}"> 
+@endsection
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+@endsection
+@section('main_content')
+    <div class="container-fluid">
+       
+        <div class="row">
+            <div class="card card-primary col-md-12">
+                <div class="card-header row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h3 class="card-title pull-left">{{ $type }} Order List </h3>
+                            </div>
+                            <div class="col-md-12">
+                                @if (session('success'))
+                                    <span class="alert alert-success d-block" role="alert">
+                                        <strong>{{ session('success') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- /.card-header -->
+
+                <div class="card-body">
+                    
+                       
+                        <table id="example" class="table table-bordered table-hover" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>SI</th>
+                                    <th>Name</th>
+                                    <th>Merit Point</th>
+                                    <th>Mobile</th>
+                                    <th>Date</th>
+                                    <th>Hour</th>
+                                    <th>People</th>
+                                    <th>Special Request</th>
+                                    <th>Accepte</th>
+                                    <th></th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @isset($data)
+                                    
+                                    @foreach ($data as $item)
+                                        <tr>
+                                            <td>{{$loop->iteration}}</td>
+                                            <td>{{$item->fName." ".$item->lname}}</td>
+                                            <td>{{$item->user_point}}</td>
+                                            <td>{{$item->mobile}}</td>
+                                            <td>{{$item->date}}</td>
+                                            <td>{{$item->hour}}</td>
+                                            <td>{{$item->people}}</td>
+                                            <td>{{$item->specialReq}}</td>
+                                            <td ><a class="btn btn-success" href="{{ route('order-confirm',['Yes',$item->id]) }}">Yes</a> </td>
+
+                                            <td><a class="btn btn-danger" href="{{ route('order-confirm',['No',$item->id]) }}">No</a></td>
+
+                                        </tr>
+                                    @endforeach
+                                @endisset
+                            </tbody>
+                        </table>
+                   
+
+                </div>
+
+            </div>{{-- card end --}}
+            {{-- </div> --}}
+        </div><!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+
+@endsection
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="{{ asset('js/order/index.js') }}"></script>
+    <script src="{{ asset('js/jquery.print.js') }}"></script>
+
+    {{-- data table --}}
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+@endsection
