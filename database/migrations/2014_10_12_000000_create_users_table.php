716804a7c9d2823c6d20fb9b6a45300e37253b9c
nafischonchol <nafisislam161@gmail.com>
2021-10-22 04:04:36 +0600

    add new column for user merit point

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->integer('user_point')->default(100);
+
+
             $table->rememberToken();
             $table->timestamps();
         });
